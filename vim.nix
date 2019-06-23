8dfddea790bf0918935e5b8bb27ad450184ac59b
k-solutions <h.l.kochev@gmail.com>
2019-06-23 17:46:52 +0300

    added vim config for home-manager

--- vim.nix
+++ vim.nix
@@ -73,30 +73,31 @@ vim_configurable.customize {
     # Use the default plugin list shipped with nixpkgs
     vimrcConfig.vam.knownPlugins = pkgs.vimPlugins;
     vimrcConfig.vam.pluginDictionaries = [
-        { names = [
-            # Here you can place all your vim plugins
-            # They are installed managed by `vam` (a vim plugin manager)
-            "Syntastic"
-            # "ctrlp"
-            "vim-airline"
-            "vim-addon-nix"
-            # "taglist"
-            "vim-airline"
-            "vim-nix"
-            "vim-closetag"
-
-            # Github plugins
-            # "github:LnL7/vim-nix"
-            # "github:alvan/vim-closetag"
-            "vim-css-color"
-            "elm-vim"
-            "vim-colorschemes"
-            "floobits-neovim"
-            "vim2hs"
-            "vim-hdevtools"
-            "vim-stylishask"
-            "vim-elixir"
-            # "vim-mix-format"
-        ]; }
+      { names = import ./vimPlugins.nix; } 
+      #      [
+      #            # Here you can place all your vim plugins
+      #            # They are installed managed by `vam` (a vim plugin manager)
+      #            "Syntastic"
+      #            # "ctrlp"
+      #            "vim-airline"
+      #            "vim-addon-nix"
+      #            # "taglist"
+      #            "vim-airline"
+      #            "vim-nix"
+      #            "vim-closetag"
+      #
+      #            # Github plugins
+      #            # "github:LnL7/vim-nix"
+      #            # "github:alvan/vim-closetag"
+      #            "vim-css-color"
+      #            "elm-vim"
+      #            "vim-colorschemes"
+      #            "floobits-neovim"
+      #            "vim2hs"
+      #            "vim-hdevtools"
+      #            "vim-stylishask"
+      #            "vim-elixir"
+      #            # "vim-mix-format"
+      #        ]; }
       ];
     }
---
+++ vimPlugins.nix
@@ -0,0 +1,25 @@
+[            
+    # Here you can place all your vim plugins
+    # They are installed managed by `vam` (a vim plugin manager)
+    "Syntastic"
+    # "ctrlp"
+    "vim-airline"
+    "vim-addon-nix"
+    # "taglist"
+    "vim-airline"
+    "vim-nix"
+    "vim-closetag"
+
+    # Github plugins
+    # "github:LnL7/vim-nix"
+    # "github:alvan/vim-closetag"
+    "vim-css-color"
+    "elm-vim"
+    "vim-colorschemes"
+    "floobits-neovim"
+    "vim2hs"
+    "vim-hdevtools"
+    "vim-stylishask"
+    "vim-elixir"
+    # "vim-mix-format"
+]
